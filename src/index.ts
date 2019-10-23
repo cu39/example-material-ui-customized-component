d97438938694bfcb15950d4e06d27dcc06b76a3c
Shusaku NAKAZATO <cu393uc@gmail.com>
2019-10-24 02:09:58 +0900

    Introduce React
    - yarn add -D react react-dom @types/react @types/react-dom
    - add src/ts/index.tsx, imported in src/index.ts
    - enable jsx option in tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-class ExampleClass {
-  private greeting: string
-
-  constructor(greeting: string) {
-    this.greeting = greeting
-  }
-
-  greet(name: string) {
-    return `${this.greeting} ${name}!`
-  }
-}
-
-const eg = new ExampleClass('Hello')
-console.log(eg.greet('John'))
+import './ts'
